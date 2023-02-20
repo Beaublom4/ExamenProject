e3b6a4e4fb7e3e3633e2e1e8db07e30f7fe3696b
Gerran Roord <GerranRoord@gmail.com>
2023-02-20 11:03:42 +0100

    changes animator paramator check
    changed animator paramator check for walk animation to fixedUpdate

--- Examen Project/Assets/Scripts/Player/PlayerMovement.cs
+++ Examen Project/Assets/Scripts/Player/PlayerMovement.cs
@@ -9,6 +9,7 @@ public class PlayerMovement : MonoBehaviour
     public Animator anim;
     public bool canMove;
 
+
     // Update is called once per frame
     void Update()
     {
@@ -17,21 +18,23 @@ public class PlayerMovement : MonoBehaviour
             //reads WASD inputs.
             movement = new Vector3(Input.GetAxisRaw("Horizontal"), 0, Input.GetAxisRaw("Vertical"));
             moveCharacter(movement);
-
-            //changes the paramater direction of the animator.
-            if (movement == new Vector3(0,0,0))
-                anim.SetInteger("direction", 0);
-            else if (movement.x > 0)
-                anim.SetInteger("direction", 3);
-            else if (movement.x < 0)
-                anim.SetInteger("direction", 4);
-            if (movement.z > 0)
-                anim.SetInteger("direction", 2);
-            else if (movement.z < 0)
-                anim.SetInteger("direction", 1);
         }
     }
 
+    private void FixedUpdate()
+    {
+        //changes the paramater direction of the animator.
+        if (movement == new Vector3(0, 0, 0))
+            anim.SetInteger("direction", 0);
+        else if (movement.x > 0)
+            anim.SetInteger("direction", 3);
+        else if (movement.x < 0)
+            anim.SetInteger("direction", 4);
+        if (movement.z > 0)
+            anim.SetInteger("direction", 2);
+        else if (movement.z < 0)
+            anim.SetInteger("direction", 1);
+    }
     void moveCharacter(Vector3 direction)
     {
         //moves the player in the right direction times speed.
