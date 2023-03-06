28f9b3b7b69ac30eb2a4043920b7165cb75c0fae
Sander Y B <sanderybrinkman@live.nl>
2023-03-06 12:38:07 +0100

    Screens updated
    + navmesh in game scene
    + TP blocks added onto screens

--- Examen Project/Assets/Scripts/Screen/LevelSwitching.cs
+++ Examen Project/Assets/Scripts/Screen/LevelSwitching.cs
@@ -22,4 +22,38 @@ public class LevelSwitching : MonoBehaviour
             collision.transform.position = newPlayerpos.transform.position;
         }
     }
+
+    private void OnDrawGizmosSelected()
+    {
+        Gizmos.color = Color.red;
+        Gizmos.DrawCube(transform.position, transform.localScale);
+
+        if (newPlayerpos != null)
+        {
+            Gizmos.DrawSphere(newPlayerpos.transform.position, 0.5f);
+        }
+
+        if (newcamPos != null)
+        {
+            Gizmos.color = Color.grey;
+            Gizmos.DrawSphere(newcamPos.transform.position, 0.5f);
+        }
+    }
+
+    private void OnDrawGizmos()
+    {
+        Gizmos.color = Color.blue;
+        Gizmos.DrawCube(transform.position, (transform.localScale -(Vector3.one / 10)));
+
+        if (newPlayerpos != null)
+        {
+            Gizmos.DrawSphere(newPlayerpos.transform.position, 0.3f);
+        }
+
+        if (newcamPos != null)
+        {
+            Gizmos.color = Color.black;
+            Gizmos.DrawSphere(newcamPos.transform.position, 0.3f);
+        }
+    }
 }
