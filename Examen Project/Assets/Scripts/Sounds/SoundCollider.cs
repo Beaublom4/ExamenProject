ee799112f60af499a60bdf146d1a521735e86e97
Beaublom4 <beaublom4@gmail.com>
2023-03-27 15:25:22 +0200

    SoundCollider directional light toggle added

--- Examen Project/Assets/Scripts/Sounds/SoundCollider.cs
+++ Examen Project/Assets/Scripts/Sounds/SoundCollider.cs
@@ -5,9 +5,12 @@ using UnityEngine;
 public class SoundCollider : MonoBehaviour
 {
     public AudioClip musicClip, backgroundClip;
+    public GameObject directionalLight;
+    public bool directionalLightToggle;
 
     private void OnTriggerEnter(Collider other)
     {
         SoundManager.Instance.SwitchSoundTrack(musicClip, backgroundClip);
+        directionalLight.SetActive(directionalLightToggle);
     }
 }
